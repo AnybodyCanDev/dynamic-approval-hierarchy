d7b9fd3711dd7d26b9acbcace47bba8976aea7c0
keya161 <kkesani@gmail.com>
2025-03-26 12:04:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # config.py
-ZOHO_ORG_ID = "60038600013"
-ZOHO_CLIENT_ID = "1000.EQ66ZF5CU8YC5MT45RCOFGNE5BVQBK"
-ZOHO_CLIENT_SECRET = "9a0f33a2d327203fa6c74f0efc04f1222b2e607f46"
+ZOHO_ORG_ID = ""
+ZOHO_CLIENT_ID = ""
+ZOHO_CLIENT_SECRET = ""
 # Initially obtained access token
-ZOHO_ACCESS_TOKEN = "1000.'1000.c8651ccfb7e32dceac58a2e6882d469e.7b43f1cde86667ef474ac0253ee80f83', .d95744555128f6de793307de0b332f3e"
+ZOHO_ACCESS_TOKEN = ""
 # Your stored refresh token (which you don't expect to change often)
-ZOHO_REFRESH_TOKEN = "1000.95c0693346ebb09b3b1944efe2a5ee51.b7f75d16f573ab766522811bbbd8feb9"
+ZOHO_REFRESH_TOKEN = ""
 
 # API endpoints
-ZOHO_API_DOMAIN = "https://www.zohoapis.in"
-BILLS_ENDPOINT = "https://www.zohoapis.in/books/v3/bills/2353408000000035041?organization_id=60038600013"
-TOKEN_REFRESH_URL = "https://accounts.zoho.in/oauth/v2/token"
+ZOHO_API_DOMAIN = ""
+BILLS_ENDPOINT = ""
+TOKEN_REFRESH_URL = ""
 
 # Set the token expiry (in seconds); typically, it's 3600 seconds (1 hour)
 ACCESS_TOKEN_LIFESPAN = 3600
